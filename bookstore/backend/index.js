26b217a5282a9791cd5b1cae200d5a020b45897c
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-06-20 23:29:35 +0800

    cors policy fixed

--- bookstore/backend/index.js
+++ bookstore/backend/index.js
@@ -2,11 +2,24 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// allow all origins
+app.use(cors());
+
+// allow only custom origin with liimited methods and content header
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (request, response) => {
   console.log(request);
